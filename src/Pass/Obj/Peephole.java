e4772fd6e89a3aa72d5c1885f7d2ab8a873b5843
echo17666 <72566693+echo17666@users.noreply.github.com>
2023-07-26 16:57:16 +0800

    一些窥孔优化

--- src/Pass/Obj/Peephole.java
+++ src/Pass/Obj/Peephole.java
@@ -2,9 +2,9 @@ package Pass.Obj;
 import Backend.component.ObjBlock;
 import Backend.component.ObjFunction;
 import Backend.component.ObjModule;
-import Backend.instruction.ObjBinary;
-import Backend.instruction.ObjInstr;
+import Backend.instruction.*;
 import Backend.operand.ObjImm;
+import Backend.operand.ObjImm12;
 import IR.Value.BasicBlock;
 import IR.Value.Instructions.Instruction;
 import Pass.Pass;
@@ -26,6 +26,9 @@ public class Peephole implements Pass.ObjPass{
             for(IList.INode<ObjBlock,ObjFunction> instBlock : blocks){
                 ObjBlock block = instBlock.getValue();
                 zero(block);
+                multiple(block);
+                uselessMove(block);
+                uselessJump(block,instBlock.getNext());
             }
         }
     }
@@ -34,18 +37,106 @@ public class Peephole implements Pass.ObjPass{
         for(IList.INode<ObjInstr, ObjBlock> instNode : objBlock.getInstrs()){
             ObjInstr nowInstr=instNode.getValue();
             if(nowInstr instanceof ObjBinary nowBinary){
-                if(nowBinary.isSrc2Imm()){
-                    ObjImm nowImm = (ObjImm)nowBinary.getSrc2();
-                    if(nowImm.getImmediate() == 0){
+                if(nowBinary.isSrc2Imm12()){
+                    ObjImm12 nowImm = (ObjImm12)nowBinary.getSrc2();
+                    if(nowImm.getImmediate12() == 0){
                         if(nowBinary.getDst().equals(nowBinary.getSrc1())){//相同
-                            //delete
+                            instNode.removeFromList();
                         }
-                        else{
-//                            nowBinary.setType("mov");//改成move
+                        else if(nowBinary.getType().equals("add")||nowBinary.getType().equals("sub")){
+                            ObjMove objMove=new ObjMove(nowBinary.getDst(),nowBinary.getSrc1());
+                            IList.INode<ObjInstr, ObjBlock> newNode = new IList.INode<ObjInstr, ObjBlock>(objMove);
+                            newNode.insertBefore(instNode);
+                            instNode.removeFromList();
                         }
+                    }
+                }
+            }
+            else if(nowInstr instanceof ObjMove objMove){
+                if(objMove.getDst().equals(objMove.getSrc())){
+                    instNode.removeFromList();
+                }
+            }
+        }
+    }
+    /* 乘法优化 mul t0, t1, 19 => sll t2, t1, 5
+                                sll t3, t1, 1
+                                add t2, t2, t3
+                                sll t3, t1, 0
+                                add t2, t2, t3
+                                mov t0, t2
+     */
+    private void multiple(ObjBlock objBlock){
+        for(IList.INode<ObjInstr, ObjBlock> instNode: objBlock.getInstrs()){
+            ObjInstr nowInstr = instNode.getValue();
+            if(nowInstr instanceof ObjBinary nowBinary&&nowBinary.getType().equals("mul")){
+                if(nowBinary.isSrc2Imm12()){
+                    ObjImm12 nowImm = (ObjImm12)nowBinary.getSrc2();
+                    int num=nowImm.getImmediate12();
+                    String biNum="";
+                    while(num>0){
+                        biNum+=num%2;
+                        num/=2;
+                    }
+                    String one="1";
+                    String zero="0";
+                    String oneString=biNum.replace(zero,"");
+                    if(oneString.length()>biNum.length()/2){//加法换1
+
+
+                    }
+                    else{
+                        //减法换0
                     }
                 }
             }
         }
     }
+    /* move t0, t1
+       move t0, t2 => move t0, t2
+     */
+    private void uselessMove(ObjBlock objBlock){
+        for(IList.INode<ObjInstr, ObjBlock> instNode: objBlock.getInstrs()){
+            ObjInstr nowInstr = instNode.getValue();
+            IList.INode<ObjInstr, ObjBlock> nextNode=instNode.getNext();
+            if(nowInstr instanceof ObjMove objMove){
+                if(nextNode!=null&&nextNode.getValue() instanceof ObjMove nextMove){
+                    if(objMove.getDst().equals(nextMove.getDst()) && !(nextMove.getDst().equals(nextMove.getSrc()))){
+                        instNode.removeFromList();
+                    }
+                }
+            }
+        }
+    }
+    /* j aaa
+       aaa:  => null
+     */
+    private void uselessJump(ObjBlock objBlock, IList.INode<ObjBlock,ObjFunction> nextBlock){
+        for(IList.INode<ObjInstr,ObjBlock> instNode: objBlock.getInstrs()){
+            ObjInstr nowInstr=instNode.getValue();
+            if(nowInstr instanceof ObjBranch objBranch){
+                if(nextBlock!=null && !(objBranch.getCondType()) && objBranch.getTarget().equals(nextBlock.getValue())){
+                    instNode.removeFromList();
+                }
+            }
+        }
+    }
+    /* store a, c(d)   store a, c(d)
+       load b, c(d) => mov b, a
+     */
+    private void uselessMemory(ObjBlock objBlock){
+        for(IList.INode<ObjInstr,ObjBlock> instNode: objBlock.getInstrs()){
+            ObjInstr nowInstr=instNode.getValue();
+            IList.INode<ObjInstr,ObjBlock> nextNode=instNode.getNext();
+            if(nextNode!=null &&nowInstr instanceof ObjStore objStore &&nextNode.getValue() instanceof ObjLoad objLoad){
+                if(objStore.getAddr().equals(objLoad.getAddr())&&objStore.getOffset().equals(objLoad.getOffset())){
+                    ObjMove newMove=new ObjMove(objLoad.getDst(),objStore.getSrc());
+                    IList.INode<ObjInstr, ObjBlock> newNode = new IList.INode<ObjInstr, ObjBlock>(newMove);
+                    newNode.insertAfter(instNode);
+                    nextNode.removeFromList();
+                }
+            }
+        }
+    }
+
 }
